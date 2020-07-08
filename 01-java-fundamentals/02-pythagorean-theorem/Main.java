307e1dbe87ec0d14fcb3aecc36053f069a68d406
May <mayyuhein@gmail.com>
2020-07-08 16:21:39 -0400

    Pythagorean Theorem

---
+++ 01-java-fundamentals/02-pythagorean-theorem/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String args[]) {
+        Pythagorean pythagorean = new Pythagorean();
+        System.out.println(pythagorean.calculateHypotenuse(5, 20));
+    }
+}
